5ad60283bb8d6a2d5d503dd6fba35c79e25275a1
renzi1 <andy.ren@mail.utoronto.ca>
2023-11-13 12:34:25 -0500

    added constructor and methods for user.java

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
@@ -29,7 +30,27 @@ public class User {
     public void changePlaylist() {}
 
     // Two constructors needed for creating the User and for user id.
-//    public User() {}
+    public User(String photo, String name, int age, String bio, Playlist spotifyPlaylist) {
+        this.photo = photo;
+        this.age = age;
+        this.bio = bio;
+        this.name = name;
+        this.spotifyPlaylist = spotifyPlaylist;
+        this.friends = new ArrayList<>();
+        this.closeFriends = new ArrayList<>();
+    }
+
+    public void change_photo(String new_photo) {
+        this.photo = new_photo;
+    }
+
+    public void change_bio(String new_bio) {
+        this.bio = new_bio;
+    }
+
+    public void change_playlist(Playlist new_playlist) {
+        this.spotifyPlaylist = new_playlist;
+    }
 //
 //    public User() {}
 
