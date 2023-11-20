500867e3e4982a21c1cd2482a68bc513f9ee1509
Yaymichael <m.pan@mail.utoronto.ca>
2023-11-20 17:43:14 -0500

    Implemented decline_invite under interface_adapter

--- src/interface_adapter/decline_invite/DeclinePresenter.java
+++ src/interface_adapter/decline_invite/DeclinePresenter.java
@@ -1,4 +1,19 @@
 package interface_adapter.decline_invite;
 
-public class DeclinePresenter {
+import interface_adapter.inbox.InboxState;
+import interface_adapter.inbox.InboxViewModel;
+import use_case.decline_invite.DeclineOutputBoundary;
+import use_case.decline_invite.DeclineOutputData;
+
+public class DeclinePresenter implements DeclineOutputBoundary {
+    private final InboxViewModel inboxViewModel;
+    public DeclinePresenter(InboxViewModel inboxViewModel) { this.inboxViewModel = inboxViewModel; }
+
+    @Override
+    public void prepareView(String lst) {
+        InboxState inboxState = inboxViewModel.getState();
+        inboxState.setDel(lst);
+        inboxViewModel.firePropertChanged();
+    }
+
 }
--- src/interface_adapter/decline_invite/DeclineState.java
+++ src/interface_adapter/decline_invite/DeclineState.java
@@ -1,4 +1,10 @@
 package interface_adapter.decline_invite;
 
 public class DeclineState {
+    private String msg = "";
+    public DeclineState(DeclineState copy) { msg = copy.msg; }
+    public DeclineState() {
+    }
+    public String getMsg() { return msg; }
+    public void setMsg(String s) { this.msg = s; }
 }
