20ad40b1a15928438b0b7e172706fbe886ae70d7
Yaymichael <m.pan@mail.utoronto.ca>
2023-11-20 12:45:32 -0500

    Implemented accept_invite under use_case

--- src/use_case/accept_invite/AcceptOutputBoundary.java
+++ src/use_case/accept_invite/AcceptOutputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.accept_invite;
 
 public interface AcceptOutputBoundary {
+    void prepareView(String lst);
 }
--- src/use_case/accept_invite/AcceptOutputData.java
+++ src/use_case/accept_invite/AcceptOutputData.java
@@ -1,4 +1,9 @@
 package use_case.accept_invite;
 
 public class AcceptOutputData {
+    private final String msg;
+
+    public AcceptOutputData(String message) { this.msg = message; }
+
+    public String getMsg() { return msg; }
 }
