c725db0898af50ca2fa67f39d22b58b23dd5857b
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2023-11-20 18:19:27 -0500

    Implemented the basic structure of playlist and made the categorize_playlist method.

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -1,13 +1,35 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Playlist {
     private String url;
 
     private CommonUser owner;
 
+    public HashMap<String, Integer> categorize_Playlist() {
+        user_playlist = csv.grab_playlist();
+        HashMap<String, Integer> ans = new HashMap<String, Integer>();
+        for(Track track : user_playlist){
+            for(String attribute: track.genres){
+                if(ans.containsKey(attribute)){
+                    ans.put(attribute,ans.get(attribute)+1);
+                }
+                else{
+                    ans.put(attribute,1);
+                }
+            }
+        }
+        return ans;
+    }
+
     public ArrayList<CommonUser> matchOtherPlaylist() {
-        throw new UnsupportedOperationException();
+        // TODO: check that this works
+        all_users = csv.grab();
+        for(CommonUser user : allusers){
+            user_playlist_dict = user;
+
+        }
     }
 }
