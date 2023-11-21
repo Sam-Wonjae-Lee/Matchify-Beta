ea77af9c190888c4bb6a95f9e4f01681cffda41f
renzi1 <andy.ren@mail.utoronto.ca>
2023-11-21 13:24:08 -0500

    Added match files plus some modifications to user and commonuser

--- src/entity/User.java
+++ src/entity/User.java
@@ -7,11 +7,11 @@ public interface User {
     String getName();
     String getPicture();
     void setPicture(String photo);
-    Playlist getPlaylist();
+    int getPlaylist();
     ArrayList<Friend> getFriendList();
     ArrayList<String> getInbox();
     void addFriend(int userID, String name);
-    void setPlaylist(Playlist playlist);
+//    void setPlaylist(Playlist playlist);
     void addFriendRequest(String request);
 
 }
---
+++ src/interface_adapter/Match/MatchPresenter.java
@@ -0,0 +1,4 @@
+package interface_adapter.Match;
+
+public class MatchPresenter {
+}
