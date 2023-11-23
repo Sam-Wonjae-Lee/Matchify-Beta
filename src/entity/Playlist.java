804c0946ce6bab8e314f2955e0d474d5efbf1b4a
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2023-11-23 15:42:59 -0500

    Implemented the compare_playlist function for the Playlist class.

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -24,9 +24,33 @@ public class Playlist {
         return ans;
     }
 
+    public int compare_playlist(Playlist other_playlist){
+        HashMap<String, Integer> other_category = other_playlist.categorize_Playlist();
+        HashMap<String, Integer> current_category = this.categorize_Playlist();
+        int ans = 0;
+        for(String key : current_category.keySet()){
+            if(other_category.containsValue(key)){
+                int val1 = other_category.get(key);
+                int val2 = current_category.get(key);
+                ans += Math.max(val1,val2) - Math.min(val1, val2);
+            }
+            else{
+                ans -= current_category.get(key);
+            }
+        }
+        for(String key : other_category.keySet()){
+            if(current_category.containsValue(key)){
+                ans -= other_category.get(key);
+            }
+        }
+        return ans;
+
+    }
+
     public ArrayList<CommonUser> matchOtherPlaylist() {
         // TODO: check that this works
-        all_users = csv.grab();
+        all_users = csv.grab_all_user();
+        current_playlist_interests = this.owner.getPlaylist().categorize_Playlist();
         for(CommonUser user : allusers){
             user_playlist_dict = user;
 
