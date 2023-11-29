4d1e23883fcf7f38a6c6ee5bfa6c02107676c94a
renzi1 <andy.ren@mail.utoronto.ca>
2023-11-29 00:45:20 -0500

    Added View files, implemented parts of MatchView and MatchViewModel

--- src/interface_adapter/Match/MatchViewModel.java
+++ src/interface_adapter/Match/MatchViewModel.java
@@ -1,4 +1,28 @@
 package interface_adapter.Match;
 
-public class MatchViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class MatchViewModel extends ViewModel {
+    public static final String FOLLOW_BUTTON_LABEL = "Follow";
+    public final String TITLE = "Matches";
+
+    public MatchViewModel(String viewName) {
+        super(viewName);
+    }
+
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+
+    @Override
+    public void firePropertyChanged() {
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
 }
