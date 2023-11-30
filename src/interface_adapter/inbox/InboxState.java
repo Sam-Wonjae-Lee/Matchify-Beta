a5bf874b0c959412df34ed4f3fa0926e9596284d
davidli <dvd.li@mail.utoronto.ca>
2023-11-30 17:48:06 -0500

    added view manager model and view manager

--- src/interface_adapter/inbox/InboxState.java
+++ src/interface_adapter/inbox/InboxState.java
@@ -1,11 +1,15 @@
 package interface_adapter.inbox;
 
+import java.util.ArrayList;
+
 public class InboxState {
 
+    public ArrayList<String> inbox = new ArrayList<>();
     private String username = "";
 
     public InboxState(InboxState copy) {
         username = copy.username;
+        inbox = copy.inbox;
     }
 
     // Because of the previous copy constructor, the default constructor must be explicit.
@@ -16,5 +20,13 @@ public class InboxState {
     }
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public ArrayList<String> getInbox(){
+        return inbox;
+    }
+
+    public void setInbox(ArrayList<String> lst){
+        this.inbox = lst;
     }
 }
