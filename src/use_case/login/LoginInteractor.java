32103d444b18819b56670976f909efd1ac2a2ce3
Wonjae Lee <samuellee0220@gmail.com>
2023-12-01 03:53:53 -0500

    Implemented LoginInteractor

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -1,8 +1,26 @@
 package use_case.login;
 
 public class LoginInteractor implements LoginInputBoundary{
+
+    final LoginUserDataAccessInterface userDataAccessObject;
+    final LoginOutputBoundary loginPresenter;
+
+    public LoginInteractor(LoginUserDataAccessInterface userDataAccessInterface, LoginOutputBoundary loginOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.loginPresenter = loginOutputBoundary;
+    }
+
     @Override
     public void execute(LoginInputData loginInputData) {
-
+        String userId = loginInputData.getUserId();
+        if (userDataAccessObject.userExists(userId)) {
+            // PrepareFailView refreshes SuccessesView
+            loginPresenter.prepareFailView(userId + ": Account already exists.");
+        } else {
+            String username = userDataAccessObject.getUsername(userId);
+            String profilePictureUrl = userDataAccessObject.getProfilePictureUrl(userId);
+            LoginOutputData loginOutputData = new LoginOutputData(userId, username, profilePictureUrl, false);
+            loginPresenter.prepareSuccessView(loginOutputData);
+        }
     }
 }
