51df2d86a5d82e51185786a13d4dcaf08938dba6
Wonjae Lee <samuellee0220@gmail.com>
2023-12-01 01:32:26 -0500

    Implemented get genres and showed example code for getting all genres in hashmap from one userid

--- src/data_access/SpotifyApiCallGetInfoDataAccessObject.java
+++ src/data_access/SpotifyApiCallGetInfoDataAccessObject.java
@@ -3,12 +3,13 @@ package data_access;
 import data_access.SpotifyApiCallUserProfileDataAccessObject;
 import data_access.SpotifyApiCallUserPlaylistDataAccessObject;
 import data_access.SpotifyApiCallPlaylistItemsDataAccessObject;
+import data_access.SpotifyApiCallArtistGenresDataAccessObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.hc.core5.http.ParseException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
@@ -60,11 +61,11 @@ public class SpotifyApiCallGetInfoDataAccessObject implements SpotifyApiCallInte
 
     // ========== Playlist Items ==========
 
-    public static List<String> getArtistsIds(String userId) throws IOException, ExecutionException, InterruptedException, SpotifyWebApiException {
+    public static List<String> getArtistsIds(String playlistId) throws IOException, ExecutionException, InterruptedException, SpotifyWebApiException {
         String accessToken = SpotifyApiCallAccessTokenDataAccessObject.getAccessToken();
         List<String> artistIds = new ArrayList<>();
 
-        JSONObject response = SpotifyApiCallPlaylistItemsDataAccessObject.getPlaylistItems(accessToken, userId);
+        JSONObject response = SpotifyApiCallPlaylistItemsDataAccessObject.getPlaylistItems(accessToken, playlistId);
         JSONArray itemsArray = response.getJSONArray("items");
 
         for (int i = 0; i< itemsArray.length(); i++) {
@@ -84,19 +85,43 @@ public class SpotifyApiCallGetInfoDataAccessObject implements SpotifyApiCallInte
     }
 
 
+    // ========== Artist Genres ==========
 
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, SpotifyWebApiException {
-//        String username = getUsername("31x4bcq6wdza6wx7sh6pqks7u2em");
-//        System.out.println(username);
+    public static List<String> getGenres(String artistId) throws IOException, ParseException, ExecutionException, InterruptedException, SpotifyWebApiException {
+        String accessToken = SpotifyApiCallAccessTokenDataAccessObject.getAccessToken();
+        List<String> genres = new ArrayList<>();
 
-//        String profileImage = getUserProfilePicture("31x4bcq6wdza6wx7sh6pqks7u2em");
-//        System.out.println(profileImage);
+        JSONObject response = SpotifyApiCallArtistGenresDataAccessObject.getArtistGenres(accessToken, artistId);
+        JSONArray genresArray = response.getJSONArray("genres");
 
-//        List playlistIds = getPlaylistIds("ayimkorean");
-//        System.out.println(playlistIds);
+        for (int i = 0; i < genresArray.length(); i++) {
+            String genre = genresArray.getString(i);
+            genres.add(genre);
+        }
+        return genres;
+    }
 
-        List artistIds = getArtistsIds("3NQSAczjoIq6UXBps2pjW8");
-        System.out.println(artistIds);
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, SpotifyWebApiException, ParseException {
+
+
+        Map<String, Integer> allGenresFrequencyMap = new HashMap<>();
+
+        List<String> playlistIds = getPlaylistIds("31s453ebjxsfte4fzcyqkanmrlb4");
+        for (String playlistId : playlistIds) {
+            List<String> artistIds = getArtistsIds(playlistId);
+
+            for (String artistId : artistIds) {
+                List<String> genres = getGenres(artistId);
+
+                for (String genre : genres) {
+                    allGenresFrequencyMap.put(genre, allGenresFrequencyMap.getOrDefault(genre, 0) + 1);
+                }
+            }
+
+        }
+        System.out.println(allGenresFrequencyMap);
+
 
     }
 
