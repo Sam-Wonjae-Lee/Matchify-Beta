593ff56ce80b5924453ae4a6f0d6cc201da81bc8
Yaymichael <m.pan@mail.utoronto.ca>
2023-11-30 20:45:37 -0500

    Implemented parts of the accept button

--- src/interface_adapter/accept_invite/AcceptPresenter.java
+++ src/interface_adapter/accept_invite/AcceptPresenter.java
@@ -1,4 +1,19 @@
 package interface_adapter.accept_invite;
 
-public class AcceptPresenter {
+import interface_adapter.inbox.InboxState;
+import interface_adapter.inbox.InboxViewModel;
+import use_case.accept_invite.AcceptOutputBoundary;
+
+public class AcceptPresenter implements AcceptOutputBoundary {
+
+    private final InboxViewModel inboxViewModel;
+
+    public AcceptPresenter(InboxViewModel inboxViewModel) { this.inboxViewModel = inboxViewModel; }
+
+    @Override
+    public void prepareView(String lst) {
+        InboxState inboxState = inboxViewModel.getState();
+        inboxState.setAdd(lst);
+        inboxViewModel.firePropertyChanged();
+    }
 }
--- src/interface_adapter/accept_invite/AcceptState.java
+++ src/interface_adapter/accept_invite/AcceptState.java
@@ -1,4 +1,20 @@
 package interface_adapter.accept_invite;
 
 public class AcceptState {
+    private String msg = "";
+
+    public AcceptState(AcceptState copy){
+        msg = copy.msg;
+    }
+
+    public AcceptState(){
+    }
+
+    public String getMsg(){
+        return msg;
+    }
+
+    public void setMsg(String s){
+        this.msg = s;
+    }
 }
