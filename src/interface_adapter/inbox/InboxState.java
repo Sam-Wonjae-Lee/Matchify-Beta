4eb43030d0b53c9bb1d33f5226e1448df64f91fe
Yaymichael <m.pan@mail.utoronto.ca>
2023-12-01 16:39:27 -0500

    Removed inbox controller and presenter

--- src/interface_adapter/inbox/InboxState.java
+++ src/interface_adapter/inbox/InboxState.java
@@ -8,25 +8,20 @@ public class InboxState {
     public List<String> inbox = new ArrayList<>();
     private String username = "";
 
-    private String del = null;
-
     public InboxState(InboxState copy) {
         username = copy.username;
         inbox = copy.inbox;
-        del = copy.del;
     }
 
     // Because of the previous copy constructor, the default constructor must be explicit.
     public InboxState() {}
 
-    public String getDel() { return del; }
+    public void setDel(String username) { this.username = username; }
 
-    public void setDel(String msg) {
-        this.del = msg;
-    }
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
