c3d7b631cc616cd6a8e6850204014f2125d830cf
Wonjae Lee <samuellee0220@gmail.com>
2023-12-01 13:52:53 -0500

    Implemented LabelTextPanel and LoginView

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,101 +1,104 @@
 package view;
 
-import se.michaelthelin.spotify.SpotifyApi;
-import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
-import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URI;
-import java.util.concurrent.ExecutionException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class LoginView {
+
+// Listener interface for receiving action events.
+// A "PropertyChange" event gets fired whenever a bean changes a "bound" property.
+public class LoginView extends JPanel implements ActionListener, PropertyChangeListener{
+
+    public final String viewName = "log in";
+    private final LoginViewModel loginViewModel;
+
+    final JTextField userIdInputField = new JTextField(15);
+    private final JLabel userIdErrorField = new JLabel();
+    final JButton logIn;
+    private final LoginController loginController;
+
+    public LoginView(LoginViewModel loginViewModel, LoginController controller) {
+
+        this.loginController = controller;
+        this.loginViewModel = loginViewModel;
+        this.loginViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Login Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelTextPanel userIdInfo = new LabelTextPanel(
+                new JLabel("Spotify User ID"), userIdInputField);
+
+        JPanel buttons = new JPanel();
+        logIn = new JButton(loginViewModel.LOGIN_BUTTON_LABEL);
+        buttons.add(logIn);
+
+        logIn.addActionListener(                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(logIn)) {
+                            LoginState currentState = loginViewModel.getState();
+
+                            loginController.execute(
+                                    currentState.getUserId()
+                            );
+                        }
+                    }
+                }
+        );
+
+        userIdInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                LoginState currentState = loginViewModel.getState();
+//                currentState.s(usernameInputField.getText() + e.getKeyChar());
+                loginViewModel.setState(currentState);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(title);
+        this.add(userIdInfo);
+        this.add(userIdErrorField);
+        this.add(buttons);
+
+    }
+
+    /**
+     * React to a button click that results in evt.
+     */
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Click " + evt.getActionCommand());
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        LoginState state = (LoginState) evt.getNewValue();
+        setFields(state);
+    }
+
+    private void setFields(LoginState state) {
+        userIdInputField.setText(state.getUserId());
+    }
+
 
 }
 
-//public class LoginView extends JFrame{
-//
-//    // ClientId, ClientSecret, RedirectURI - necessary info for using API.
-//    private String CLIENT_ID = "9ed5f6af048844e4851425fbc416ae10";
-//    private String CLIENT_SECRET = "df75314d40634c9db0d1da481a2302e8";
-//    private String REDIRECT_URI = "http://localhost:8888/callback";
-//
-//    private SpotifyApi spotifyApi;
-//
-//    public LoginView() {
-//
-//        // JFrame window title
-//        super("Spotify Login");
-//
-//        spotifyApi = new SpotifyApi.Builder()
-//                .setClientId(CLIENT_ID)
-//                .setClientSecret(CLIENT_SECRET)
-//                .setRedirectUri(URI.create(REDIRECT_URI))
-//                .build();
-//
-//        // Window will close to when clicking on X button.
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        // Set window size
-//        setSize(300, 200);
-//        // Set centre of screen
-//        setLocationRelativeTo(null);
-//
-//        // Button for login
-//        JButton loginButton = new JButton("Log in with Spotify");
-//        loginButton.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                openSpotifyLoginWindow();
-//            }
-//        });
-//
-//        getContentPane().setLayout(new BorderLayout());
-//        getContentPane().add(loginButton, BorderLayout.CENTER);
-//    }
-//
-//    private void openSpotifyLoginWindow() {
-//        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri().build();
-//        URI uri = authorizationCodeUriRequest.executeAsync().join();
-//
-//        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-//            try {
-//                Desktop.getDesktop().browse(uri);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        String authorizationCode = JOptionPane.showInputDialog("Enter the authorization code: ");
-//        try {
-//             /*
-//             * Retrieves the access token. The access token is a string which contains the credentials and permissions that can be used to access resources.
-//             * The access token is valid for 1 hour. After that time, the token expires and you need to request a new one.
-//             * More info is located here: https://developer.spotify.com/documentation/web-api/concepts/access-token
-//             * */
-//            AuthorizationCodeCredentials authorizationCodeCredentials = spotifyApi.authorizationCode(authorizationCode).build().executeAsync().get();
-//            System.out.println("Access Token: " + authorizationCodeCredentials.getAccessToken());
-//            System.out.println("Refresh Token " + authorizationCodeCredentials.getRefreshToken());
-//        } catch (InterruptedException | ExecutionException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    /**
-//     * Retrieves the access token. The access token is a string which contains the credentials and permissions that can be used to access resources.
-//     * The access token is valid for 1 hour. After that time, the token expires and you need to request a new one.
-//     * More info is located here: https://developer.spotify.com/documentation/web-api/concepts/access-token
-//     *
-//     * @return A string containing the temporary access token.
-//     * @throws Exception if access token cannot be retrieved.
-//     */
-//    public static void main(String[] args) {
-//        SwingUtilities.invokeLater(() -> {
-//            new LoginView().setVisible(true);
-//        });
-//    }
-//
-//}
+
