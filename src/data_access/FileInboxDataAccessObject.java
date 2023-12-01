345db804b5d9d88502574b9c739de622933a14b7
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2023-12-01 17:51:43 -0500

    made it properly

--- src/data_access/FileInboxDataAccessObject.java
+++ src/data_access/FileInboxDataAccessObject.java
@@ -1,15 +1,20 @@
 package data_access;
 
+import use_case.decline_invite.DeclineUserDataAccessInterface;
+import use_case.open_inbox.OpenInboxUserDataAccessInterface;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 // do I need to implement the SignUpUserDataAccessInterface or LoginUserDataAccessInterface?
-public class FileInboxDataAccessObject {
+public class FileInboxDataAccessObject implements DeclineUserDataAccessInterface, OpenInboxUserDataAccessInterface {
 
     private final String csvFile_path = "src/csv_files/user_friends.csv";
 
-    private HashMap<Integer, ArrayList<Integer>> data_saved = new HashMap<>();
+    private HashMap<String, HashSet<String>> data_saved = new HashMap<>();
 
     private final String sample = ",";
 
@@ -19,21 +24,17 @@ public class FileInboxDataAccessObject {
     }
 
     // read method is used for initializing the database
-    private HashMap<Integer, ArrayList<Integer>> read(){
-        // [user_id, user_name, photo, age, bio]
+    private HashMap<String, HashSet<String>> read(){
         String mystring;
-        HashMap<Integer, ArrayList<Integer>> ans = new HashMap<>();
+        HashMap<String, HashSet<String>> ans = new HashMap<>();
         try
         {
             BufferedReader brdrd = new BufferedReader(new FileReader(this.csvFile_path));
             while ((mystring = brdrd.readLine()) != null)  //Reads a line of text
             {
                 String[] users = mystring.split(sample);
-                ArrayList<Integer> new_arr = new ArrayList<>();
-                for(int a = 0; a < users.length; a++){
-                    new_arr.add(Integer.parseInt(users[a]));
-                }
-                ans.put(Integer.parseInt(users[0]), new_arr);
+                HashSet<String> new_arr = new HashSet<String>(Arrays.asList(users));
+                ans.put(users[0], new_arr);
             }
         }
         catch (IOException e)//catches exception in the try block
@@ -45,12 +46,11 @@ public class FileInboxDataAccessObject {
 
     private void write(){
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile_path))) {
-            for (Integer key : this.data_saved.keySet()) {
+            for (String key : this.data_saved.keySet()) {
                 String new_str = String.valueOf(key);
                 System.out.println(this.data_saved);
-                ArrayList<Integer> val = this.data_saved.get(key);
-                for(int i = 0; i < val.size(); i++){
-                    new_str = new_str.concat(","+String.valueOf(val.get(i)));
+                for(String val : this.data_saved.get(key)){
+                    new_str = new_str.concat(","+val);
                 }
                 writer.write(new_str);
                 writer.newLine();
@@ -60,10 +60,10 @@ public class FileInboxDataAccessObject {
         }
     }
 
-    public void add_friend(Integer user_id, Integer friend_id){
+    public void add_friend(String user_id, String friend_id){
         // adds the user into the database, if user_id already exists in the database, update it's values instead
         if(this.data_saved.containsKey(user_id)){
-            ArrayList<Integer> new_arr = this.data_saved.get(user_id);
+            HashSet<String> new_arr = this.data_saved.get(user_id);
             if(!new_arr.contains(friend_id)){
                 // it does not contain duplicate
                 new_arr.add(friend_id);
@@ -71,14 +71,27 @@ public class FileInboxDataAccessObject {
             }
         }
         else {
-            ArrayList<Integer> new_arr = new ArrayList<>();
+            HashSet<String> new_arr = new HashSet<String>();
             new_arr.add(friend_id);
             this.data_saved.put(user_id,new_arr);
         }
         this.write();
     }
 
-    public ArrayList<Integer> get_user(Integer user_id){
+    public void remove_friend(String user_id, String friend_id){
+        // removes the friend from the database, if user_id already exists in the database.
+        if(this.data_saved.containsKey(user_id)){
+            HashSet<String> new_arr = this.data_saved.get(user_id);
+            if(new_arr.contains(friend_id)){
+                // if it's in user_id's friends
+                new_arr.remove(friend_id);
+                this.data_saved.replace(user_id,new_arr);
+            }
+        }
+        this.write();
+    }
+
+    public HashSet<String> get_user(String user_id){
         // returns the values of the user_id, if user_id doesn't exist, return null instead.
         return this.data_saved.get(user_id);
     }
