bc834d8148174931b7544febde4d8754f49c078a
Sam Wonjae Lee <samuellee0220@gmail.com>
2023-11-30 20:10:04 -0500

    Created test for AccessTokenDAO

---
+++ test/data_access/SpotifyApiCallAccessTokenDataAccessObjectTest.java
@@ -0,0 +1,16 @@
+package data_access;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SpotifyApiCallAccessTokenDataAccessObjectTest{
+
+    // This test checks if the retrieved temporary access token is empty
+    @Test
+    public void testAccessTokenNotEmptyString() {
+        String accessToken = SpotifyApiCallAccessTokenDataAccessObject.getAccessToken();
+//        System.out.println(accessToken);
+        assertFalse("The access token should not be empty", accessToken.isEmpty());
+    }
+
+}
