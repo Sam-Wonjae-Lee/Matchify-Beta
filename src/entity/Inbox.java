f989a865b11039b1a5e18c1ee1b0aadefb6c4270
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2023-12-01 17:37:59 -0500

    intialized both Friendslist and Inbox entities

--- src/entity/Inbox.java
+++ src/entity/Inbox.java
@@ -2,24 +2,24 @@ package entity;
 
 import java.util.HashSet;
 
-public class FriendsList {
+public class Inbox {
+
     private HashSet<String> user_ids;
 
-    public FriendsList(){
-        // initialize FriendsList
+    public Inbox(){
         this.user_ids = new HashSet<String>();
     }
 
-    public void add_friend(String user_id){
+    public void add_invite(String user_id){
         this.user_ids.add(user_id);
     }
 
-    public void remove_friend(String user_id){
+    public void remove_invite(String user_id){
         // removes invite from inbox
         this.user_ids.remove(user_id);
     }
 
-    public HashSet<String> get_friends(){
+    public HashSet<String> get_invites(){
         return this.user_ids;
     }
 }
