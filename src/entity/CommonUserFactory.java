c6e19b05e0a81bb81bc857f18242939d8fe65d72
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2023-12-01 19:54:33 -0500

    changed Entities and FileUserDataAccessObject

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class CommonUserFactory implements UserFactory {
 
     @Override
-    public User create(String name, FriendsList friends, Inbox inbox) {
+    public CommonUser create(String name, FriendsList friends, Inbox inbox) {
         return new CommonUser(name, friends, inbox);
     }
 }
