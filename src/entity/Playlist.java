110315ed2162dc8cef140754e14aea95d1b6b9ce
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2023-12-01 19:53:13 -0500

    changed Entities and FileUserDataAccessObject

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -1,10 +1,7 @@
 package entity;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashMap;
-import org.javatuples.*;
 
 public class Playlist {
     private String url;
@@ -12,7 +9,6 @@ public class Playlist {
     private CommonUser owner;
 
     public HashMap<String, Integer> categorize_Playlist() {
-        //TODO: check this works
         user_playlist = csv.grab_playlist();
         HashMap<String, Integer> ans = new HashMap<String, Integer>();
         for(Track track : user_playlist){
@@ -29,7 +25,6 @@ public class Playlist {
     }
 
     public int compare_playlist(Playlist other_playlist){
-        //TODO: check this works
         HashMap<String, Integer> other_category = other_playlist.categorize_Playlist();
         HashMap<String, Integer> current_category = this.categorize_Playlist();
         int ans = 0;
@@ -44,7 +39,7 @@ public class Playlist {
             }
         }
         for(String key : other_category.keySet()){
-            if(!current_category.containsValue(key)){
+            if(current_category.containsValue(key)){
                 ans -= other_category.get(key);
             }
         }
@@ -55,12 +50,9 @@ public class Playlist {
     public ArrayList<CommonUser> matchOtherPlaylist() {
         // TODO: check that this works
         all_users = csv.grab_all_user();
-        HashMap<Integer, ArrayList<CommonUser>> dict = new HashMap<Integer, ArrayList<CommonUser>>();
         current_playlist_interests = this.owner.getPlaylist().categorize_Playlist();
-        Pair<Integer,String>[] arr = new Pair<Integer,String>[];
         for(CommonUser user : allusers){
-            int score = this.compare_playlist(user.getPlaylist());
-
+            user_playlist_dict = user;
 
         }
     }
