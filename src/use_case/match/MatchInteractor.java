b1838a4797f8274a7ff0e39d6399acafad40f333
renzi1 <andy.ren@mail.utoronto.ca>
2023-12-02 18:02:30 -0500

    Fixed for loop in MatchInteractor

--- src/use_case/match/MatchInteractor.java
+++ src/use_case/match/MatchInteractor.java
@@ -78,9 +78,17 @@ public class MatchInteractor implements MatchInputboundary{
         Collections.sort(sorted_keys);
 
         List<User> matchedUsers = new ArrayList<User>();
+//      This for loop adds users to List from Hashmap
         for (int keys : sorted_keys) {
+//          does not add user to list if the user is the Client
             if (!client_user.equals(ans.get(keys))) {
-                matchedUsers.add(ans.get(keys));
+//              adds to list until length is 3
+                if (matchedUsers.size() != 3) {
+                    matchedUsers.add(ans.get(keys));
+                }
+                else {
+                    break;
+                }
             }
         }
         // collection should now be sorted and idk what to do from here.
