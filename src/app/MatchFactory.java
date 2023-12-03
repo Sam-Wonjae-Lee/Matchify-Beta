660a868a3c502bade491a94ad779d28d935f4cc1
renzi1 <andy.ren@mail.utoronto.ca>
2023-12-02 19:38:41 -0500

    Added SendInviteController to MatchFactory

--- src/app/MatchFactory.java
+++ src/app/MatchFactory.java
@@ -7,21 +7,28 @@ import interface_adapter.home_page.HomePageViewModel;
 import interface_adapter.match.MatchController;
 import interface_adapter.match.MatchPresenter;
 import interface_adapter.match.MatchViewModel;
+import interface_adapter.send_invite.SendInviteController;
+import interface_adapter.send_invite.SendInvitePresenter;
 import use_case.home_page.HomePageInputBoundary;
 import use_case.home_page.HomePageInteracter;
 import use_case.home_page.HomePageOutPutBoundary;
 import use_case.match.*;
+import use_case.send_invite.SendInviteInputBoundary;
+import use_case.send_invite.SendInviteInteractor;
+import use_case.send_invite.SendInviteOutputBoundary;
+import use_case.send_invite.SendInviteUserDataAccessInterface;
 import view.MatchView;
 
 import java.io.IOException;
 
 public class MatchFactory {
 
-    public static MatchView create(ViewManagerModel viewManagerModel, MatchViewModel matchViewModel, HomePageViewModel homePageViewModel, MatchUserAccessInterface matchUserAccessInterface, MatchSpotifyAccessInterface matchSpotifyAccessInterface) throws IOException {
+    public static MatchView create(ViewManagerModel viewManagerModel, MatchViewModel matchViewModel, HomePageViewModel homePageViewModel, MatchUserAccessInterface matchUserAccessInterface, MatchSpotifyAccessInterface matchSpotifyAccessInterface, SendInviteUserDataAccessInterface sendInviteUserDataAccessInterface) throws IOException {
 
         HomePageController homePageController = createHomePageController(viewManagerModel, homePageViewModel);
         MatchController matchController = createMatchController(viewManagerModel, matchViewModel, matchUserAccessInterface, matchSpotifyAccessInterface);
-        return new MatchView(matchViewModel, matchController, homePageViewModel, homePageController);
+        SendInviteController sendInviteController= createSendInviteController(viewManagerModel, sendInviteUserDataAccessInterface);
+        return new MatchView(matchViewModel, matchController, homePageViewModel, homePageController, sendInviteController);
     }
     private static MatchController createMatchController(ViewManagerModel viewManagerModel, MatchViewModel matchViewModel, MatchUserAccessInterface matchUserAccessInterface, MatchSpotifyAccessInterface matchSpotifyAccessInterface) {
 
@@ -39,6 +46,14 @@ public class MatchFactory {
         HomePageInputBoundary homePageInteracter = new HomePageInteracter(homePagePresenter);
 
         return new HomePageController(homePageInteracter);
+    }
+
+    private static SendInviteController createSendInviteController(ViewManagerModel viewManagerModel, SendInviteUserDataAccessInterface sendInviteUserDataAccessInterface) {
+        SendInviteOutputBoundary sendInvitePresenter = new SendInvitePresenter();
+
+        SendInviteInputBoundary sendInviteInteracter = new SendInviteInteractor(sendInviteUserDataAccessInterface, sendInvitePresenter);
+
+        return new SendInviteController(sendInviteInteracter);
     }
 }
 
