b811b427124a7d653ed6224c4c8677337d032e28
davidli <dvd.li@mail.utoronto.ca>
2023-12-03 05:04:23 -0500

    finished home page factory

--- src/app/HomePageFactory.java
+++ src/app/HomePageFactory.java
@@ -1,10 +1,6 @@
 package app;
 
-import entity.CommonUserFactory;
-import entity.UserFactory;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.home_page.HomePageController;
-import interface_adapter.home_page.HomePagePresenter;
 import interface_adapter.home_page.HomePageViewModel;
 import interface_adapter.inbox.InboxViewModel;
 import interface_adapter.match.MatchController;
@@ -12,43 +8,39 @@ import interface_adapter.match.MatchPresenter;
 import interface_adapter.match.MatchViewModel;
 import interface_adapter.open_inbox.OpenInboxController;
 import interface_adapter.open_inbox.OpenInboxPresenter;
-import use_case.home_page.*;
-import use_case.match.MatchInteractor;
-import use_case.match.MatchOutputBoundary;
-import use_case.match.MatchSpotifyAccessInterface;
-import use_case.match.MatchUserAccessInterface;
+import use_case.match.*;
 import use_case.open_inbox.OpenInboxInputBoundary;
 import use_case.open_inbox.OpenInboxInteractor;
 import use_case.open_inbox.OpenInboxOutputBoundary;
 import use_case.open_inbox.OpenInboxUserDataAccessInterface;
 import view.HomePageView;
-import view.InboxView;
 
 import javax.swing.*;
 import java.io.IOException;
 
 public class HomePageFactory {
-    private HomePageFactory() {}
+    private HomePageFactory() {
+    }
 
-    public static HomePageView create (
+    public static HomePageView create(
             ViewManagerModel viewManagerModel,
             HomePageViewModel homePageViewModel,
             MatchViewModel matchViewModel,
             InboxViewModel inboxViewModel,
-            HomePageUserDataAccessInterface homePageUserDataAccessInterface,
-            HomePageSpotifyAPIDataAccessInterface homePageSpotifyAPIDataAccessInterface,
             MatchUserAccessInterface matchUserAccessInterface,
             OpenInboxUserDataAccessInterface openInboxUserDataAccessInterface,
             MatchSpotifyAccessInterface matchSpotifyAccessInterface) {
 
         try {
-            HomePageController homePageController = createHomePageUseCase(viewManagerModel, homePageViewModel, homePageSpotifyAPIDataAccessInterface, homePageUserDataAccessInterface);
-            MatchPresenter matchPresenter = new MatchPresenter(matchViewModel, viewManagerModel);
-            MatchInteractor matchInteractor = new MatchInteractor(matchPresenter, matchUserAccessInterface, matchSpotifyAccessInterface);
-            MatchController matchController = new MatchController(matchInteractor);
-            OpenInboxPresenter openInboxPresenter = new OpenInboxPresenter(inboxViewModel, viewManagerModel);
-            OpenInboxInteractor openInboxInteractor = new OpenInboxInteractor(openInboxUserDataAccessInterface, openInboxPresenter);
-            OpenInboxController openInboxController = new OpenInboxController(openInboxInteractor);
+            MatchController matchController = createMatchUseCase(
+                    viewManagerModel,
+                    matchViewModel,
+                    matchUserAccessInterface,
+                    matchSpotifyAccessInterface);
+            OpenInboxController openInboxController = createOpenInboxUseCase(
+                    viewManagerModel,
+                    inboxViewModel,
+                    openInboxUserDataAccessInterface);
             return new HomePageView(homePageViewModel, matchController, openInboxController);
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not open user data file. ");
@@ -57,21 +49,31 @@ public class HomePageFactory {
         return null;
     }
 
-    private static HomePageController createHomePageUseCase (
+    private static MatchController createMatchUseCase(
             ViewManagerModel viewManagerModel,
-            HomePageViewModel homePageViewModel,
-            HomePageSpotifyAPIDataAccessInterface homePageSpotifyAPIDataAccessInterface,
-            HomePageUserDataAccessInterface userDataAccessInterface) throws IOException {
+            MatchViewModel matchViewModel,
+            MatchUserAccessInterface userDataAccessObject,
+            MatchSpotifyAccessInterface spotifyAccessObject) throws IOException {
 
-        HomePageOutPutBoundary homePageOutPutBoundary = new HomePagePresenter(homePageViewModel, viewManagerModel);
+        MatchOutputBoundary matchOutputBoundary = new MatchPresenter(matchViewModel, viewManagerModel);
 
-        UserFactory userFactory = new CommonUserFactory();
+        MatchInputboundary matchInteractor = new MatchInteractor(
+                matchOutputBoundary,
+                userDataAccessObject,
+                spotifyAccessObject);
 
-        HomePageInputBoundary homepageInteractor = new HomePageInteractor(
-                homePageOutPutBoundary, homePageSpotifyAPIDataAccessInterface, userDataAccessInterface
-        );
+        return new MatchController(matchInteractor);
 
-        return new HomePageController(homepageInteractor);
+    }
 
+    private static OpenInboxController createOpenInboxUseCase(
+            ViewManagerModel viewManagerModel,
+            InboxViewModel inboxViewModel,
+            OpenInboxUserDataAccessInterface userDataAccessObject) throws IOException {
+        OpenInboxOutputBoundary openInboxOutputBoundary = new OpenInboxPresenter(inboxViewModel, viewManagerModel);
+
+        OpenInboxInputBoundary openInboxInputBoundary = new OpenInboxInteractor(userDataAccessObject, openInboxOutputBoundary);
+
+        return new OpenInboxController(openInboxInputBoundary);
     }
 }
