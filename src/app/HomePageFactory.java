0a72c2d80d2f5dd420a75699831327e3f1f1c065
Yaymichael <m.pan@mail.utoronto.ca>
2023-12-03 00:53:17 -0500

    Modified HomePageFactory

--- src/app/HomePageFactory.java
+++ src/app/HomePageFactory.java
@@ -6,12 +6,25 @@ import interface_adapter.ViewManagerModel;
 import interface_adapter.home_page.HomePageController;
 import interface_adapter.home_page.HomePagePresenter;
 import interface_adapter.home_page.HomePageViewModel;
+import interface_adapter.inbox.InboxViewModel;
 import interface_adapter.match.MatchController;
+import interface_adapter.match.MatchPresenter;
+import interface_adapter.match.MatchViewModel;
 import interface_adapter.open_inbox.OpenInboxController;
+import interface_adapter.open_inbox.OpenInboxPresenter;
 import use_case.home_page.HomePageInputBoundary;
 import use_case.home_page.HomePageInteracter;
 import use_case.home_page.HomePageOutPutBoundary;
+import use_case.match.MatchInteractor;
+import use_case.match.MatchOutputBoundary;
+import use_case.match.MatchSpotifyAccessInterface;
+import use_case.match.MatchUserAccessInterface;
+import use_case.open_inbox.OpenInboxInputBoundary;
+import use_case.open_inbox.OpenInboxInteractor;
+import use_case.open_inbox.OpenInboxOutputBoundary;
+import use_case.open_inbox.OpenInboxUserDataAccessInterface;
 import view.HomePageView;
+import view.InboxView;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -22,11 +35,20 @@ public class HomePageFactory {
     public static HomePageView create (
             ViewManagerModel viewManagerModel,
             HomePageViewModel homePageViewModel,
-            MatchController matchController,
-            OpenInboxController openInboxController) {
+            MatchViewModel matchViewModel,
+            InboxViewModel inboxViewModel,
+            MatchUserAccessInterface matchUserAccessInterface,
+            OpenInboxUserDataAccessInterface openInboxUserDataAccessInterface,
+            MatchSpotifyAccessInterface matchSpotifyAccessInterface) {
 
         try {
             HomePageController homePageController = createHomePageUseCase(viewManagerModel, homePageViewModel);
+            MatchPresenter matchPresenter = new MatchPresenter(matchViewModel, viewManagerModel);
+            MatchInteractor matchInteractor = new MatchInteractor(matchPresenter, matchUserAccessInterface, matchSpotifyAccessInterface);
+            MatchController matchController = new MatchController(matchInteractor);
+            OpenInboxPresenter openInboxPresenter = new OpenInboxPresenter(inboxViewModel, viewManagerModel);
+            OpenInboxInteractor openInboxInteractor = new OpenInboxInteractor(openInboxUserDataAccessInterface, openInboxPresenter);
+            OpenInboxController openInboxController = new OpenInboxController(openInboxInteractor);
             return new HomePageView(homePageViewModel, matchController, openInboxController);
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not open user data file. ");
