47e933d6ddbdec37f03160a91423dcbf29ed93ef
Yaymichael <m.pan@mail.utoronto.ca>
2023-12-03 23:23:40 -0500

    Added in memory spotify data access object

--- src/data_access/InMemorySpotifyDataAccessObject.java
+++ src/data_access/InMemorySpotifyDataAccessObject.java
@@ -1,2 +1,28 @@
-package data_access;public class InMemorySpotifyDataAccessObject {
+package data_access;
+
+import use_case.login.LoginSpotifyAPIDataAccessInterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemorySpotifyDataAccessObject implements LoginSpotifyAPIDataAccessInterface {
+
+    private final Map<String, String> userNames = new HashMap<>();
+    private final Map<String, String> profilePictures = new HashMap<>();
+    private final Map<String, Boolean> userExists = new HashMap<>();
+
+    @Override
+    public String getName(String userID) {
+        return userNames.get(userID);
+    }
+
+    @Override
+    public String getProfilePicture(String userID) {
+        return profilePictures.get(userID);
+    }
+
+    @Override
+    public boolean userExists(String user_id) {
+        return userExists.getOrDefault(user_id, false);
+    }
 }
