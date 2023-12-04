8cab2bce8a6690b034867a7fbb8f2b7bbfbaa89c
Yaymichael <m.pan@mail.utoronto.ca>
2023-12-04 02:55:55 -0500

    Added test case for accept and decline invite

---
+++ test/use_case/AcceptTest.java
@@ -0,0 +1,29 @@
+package use_case;
+
+import data_access.FileUserDataAccessObject;
+import data_access.InMemoryUserDataAccessObject;
+import entity.*;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.inbox.InboxViewModel;
+import interface_adapter.accept_invite.AcceptPresenter;
+import use_case.accept_invite.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AcceptTest {
+
+    @org.junit.Test
+    public void testSuccess() {
+        String userID = "123";
+        String friendID = "345";
+        AcceptInputData inputData = new AcceptInputData(userID, friendID);
+        AcceptUserDataAccessInterface acceptUserDataAccessInterface = new InMemoryUserDataAccessObject();
+
+        AcceptOutputBoundary successPresenter = new AcceptPresenter();
+        AcceptInteractor interactor = new AcceptInteractor(acceptUserDataAccessInterface, successPresenter);
+
+        interactor.execute(inputData);
+    }
+}
